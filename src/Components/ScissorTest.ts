286156de0bf4b749ce9ec2022f81907fc38a8623
naotaro0123 <n.ikewada@gmail.com>
2021-10-13 19:39:03 +0900

    refactor: combine functions

--- src/Components/ScissorTest.ts
+++ src/Components/ScissorTest.ts
@@ -5,23 +5,52 @@ import * as THREE from 'three';
 type SceneInfo = {
   scene: THREE.Scene;
   camera: THREE.PerspectiveCamera;
-  elem: HTMLElement | undefined;
+  element: HTMLElement | undefined;
   mesh: THREE.Mesh | undefined;
 };
+
+type DomIds = 'box' | 'pyramid';
 
 export default function ScissorTest() {
   const canvas = document.querySelector('#c');
   const renderer = new THREE.WebGLRenderer({ canvas, alpha: true });
+  render(0, renderer, [setupScene('box'), setupScene('pyramid')]);
+}
 
-  const sceneInfo1 = setupScene1();
-  const sceneInfo2 = setupScene2();
+function setupScene(domId: DomIds): SceneInfo {
+  const sceneInfo: SceneInfo = {
+    ...makeScene(),
+    element: document.querySelector(`#${domId}`),
+  };
 
-  render(0, renderer, [sceneInfo1, sceneInfo2]);
+  switch (domId) {
+    case 'box': {
+      const geometry = new THREE.BoxGeometry(1, 1, 1);
+      const material = new THREE.MeshPhongMaterial({ color: 'red' });
+      const mesh = new THREE.Mesh(geometry, material);
+      sceneInfo.scene.add(mesh);
+      sceneInfo.mesh = mesh;
+      return sceneInfo;
+    }
+    case 'pyramid': {
+      const radius = 0.8;
+      const widthSegments = 4;
+      const heightSegments = 2;
+      const geometry = new THREE.SphereGeometry(radius, widthSegments, heightSegments);
+      const material = new THREE.MeshPhongMaterial({
+        color: 'blue',
+        flatShading: true,
+      });
+      const mesh = new THREE.Mesh(geometry, material);
+      sceneInfo.scene.add(mesh);
+      sceneInfo.mesh = mesh;
+      return sceneInfo;
+    }
+  }
 }
 
 function makeScene(): SceneInfo {
   const scene = new THREE.Scene();
-
   const fov = 45;
   const aspect = 2; // the canvas default
   const near = 0.1;
@@ -37,57 +66,41 @@ function makeScene(): SceneInfo {
   light.position.set(-1, 2, 4);
   scene.add(light);
 
-  return { scene, camera, elem: undefined, mesh: undefined };
+  return { scene, camera, element: undefined, mesh: undefined };
 }
 
-function setupScene1(): SceneInfo {
-  const sceneInfo: SceneInfo = {
-    ...makeScene(),
-    elem: document.querySelector('#box'),
-  };
-  const geometry = new THREE.BoxGeometry(1, 1, 1);
-  const material = new THREE.MeshPhongMaterial({ color: 'red' });
-  const mesh = new THREE.Mesh(geometry, material);
-  sceneInfo.scene.add(mesh);
-  sceneInfo.mesh = mesh;
-  return sceneInfo;
-}
+function render(time: number = 0, renderer: THREE.WebGLRenderer, sceneInfos: SceneInfo[]) {
+  time *= 0.001;
 
-function setupScene2(): SceneInfo {
-  const sceneInfo: SceneInfo = {
-    ...makeScene(),
-    elem: document.querySelector('#pyramid'),
-  };
-  const radius = 0.8;
-  const widthSegments = 4;
-  const heightSegments = 2;
-  const geometry = new THREE.SphereGeometry(radius, widthSegments, heightSegments);
-  const material = new THREE.MeshPhongMaterial({
-    color: 'blue',
-    flatShading: true,
+  resizeRendererToDisplaySize(renderer);
+
+  renderer.setScissorTest(false);
+  renderer.clear(true, true);
+  renderer.setScissorTest(true);
+
+  sceneInfos.forEach((sceneInfo) => {
+    sceneInfo.mesh.rotation.y = time * 0.5;
+    renderSceneInfo(sceneInfo, renderer);
   });
-  const mesh = new THREE.Mesh(geometry, material);
-  sceneInfo.scene.add(mesh);
-  sceneInfo.mesh = mesh;
-  return sceneInfo;
+
+  requestAnimationFrame((timeStamp) => render(timeStamp, renderer, sceneInfos));
 }
 
 function resizeRendererToDisplaySize(renderer: THREE.WebGLRenderer): boolean {
   const canvas = renderer.domElement;
   const width = canvas.clientWidth;
   const height = canvas.clientHeight;
-  const needResize = canvas.width !== width || canvas.height !== height;
-  if (needResize) {
+  const isNeedResize = canvas.width !== width || canvas.height !== height;
+  if (isNeedResize) {
     renderer.setSize(width, height, false);
   }
-  return needResize;
+  return isNeedResize;
 }
 
 function renderSceneInfo(sceneInfo: SceneInfo, renderer: THREE.WebGLRenderer) {
-  const { scene, camera, elem } = sceneInfo;
-
+  const { scene, camera, element } = sceneInfo;
   // get the viewport relative position of this element
-  const { left, right, top, bottom, width, height } = elem.getBoundingClientRect();
+  const { left, right, top, bottom, width, height } = element.getBoundingClientRect();
 
   const isOffscreen =
     bottom < 0 ||
@@ -103,26 +116,19 @@ function renderSceneInfo(sceneInfo: SceneInfo, renderer: THREE.WebGLRenderer) {
   camera.updateProjectionMatrix();
 
   const positiveYUpBottom = renderer.domElement.clientHeight - bottom;
+  console.log(
+    'id:',
+    element.id,
+    'left:',
+    left,
+    'positiveYUpBottom:',
+    positiveYUpBottom,
+    'width:',
+    width,
+    'height:',
+    height
+  );
   renderer.setScissor(left, positiveYUpBottom, width, height);
   renderer.setViewport(left, positiveYUpBottom, width, height);
-
   renderer.render(scene, camera);
-}
-
-function render(time: number = 0, renderer: THREE.WebGLRenderer, sceneInfos: SceneInfo[]) {
-  time *= 0.001;
-
-  resizeRendererToDisplaySize(renderer);
-
-  renderer.setScissorTest(false);
-  renderer.clear(true, true);
-  renderer.setScissorTest(true);
-
-  sceneInfos[0].mesh.rotation.y = time * 0.5;
-  sceneInfos[1].mesh.rotation.y = time * 0.5;
-
-  renderSceneInfo(sceneInfos[0], renderer);
-  renderSceneInfo(sceneInfos[1], renderer);
-
-  requestAnimationFrame((timeStamp) => render(timeStamp, renderer, sceneInfos));
 }
